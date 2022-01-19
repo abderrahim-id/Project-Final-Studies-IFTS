f9689626156344e39db8df60111252452a797509
Alessio <ale00.grosso@gmail.com>
2022-01-19 16:41:08 +0100

    Ho creato una classe per connessione del db per mysql... ho messo come commento la parte per sqlite.

---
+++ src/main/java/it/etlabora/progetto/util/DbConnection.java
@@ -0,0 +1,22 @@
+package it.etlabora.progetto.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection {
+
+	public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+	//public static final String DB_DRIVER = "org.sqlite.JDBC"; per sqlite.
+	public static final String DB_NAME = "jdbc:mysql://localhost:3306/ProgettoBiblioteca";
+	//public static final String DB_NAME = "jdbc:sqlite:C:\\path\\to\\file.db"; per sqlite.
+	public static final String DB_USERNAME = "root";
+	public static final String DB_PASSWORD = "root";
+
+	public static Connection getConnection() throws Exception {
+		Connection conn = null;
+		Class.forName(DB_DRIVER);
+
+		conn = DriverManager.getConnection(DB_NAME, DB_USERNAME, DB_PASSWORD);
+		return conn;
+	}
+}
